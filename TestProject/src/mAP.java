c8a54dfe3b12311405441b85ab25525d0ca1997e
vikay2891 <bhardwajvikas93@gmail.com>
2018-06-28 23:25:49 +0530

    frst

---
+++ TestProject/src/mAP.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class mAP {
+	
+public static void main(String[] args) {
+	
+	HashMap<Integer, String> hm =new HashMap<>();
+	hm.put(101, "vikas");
+	hm.put(101, "vijay");
+	hm.put(101, "vikrant");
+	hm.put(101, "nunnas");
+	hm.put(101, "alu");
+	System.out.println(hm);
+	
+}
+
+}
